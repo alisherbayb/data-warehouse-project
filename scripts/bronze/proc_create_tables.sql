31670a210837e54d43c0fdd69675f05f21a759fd
alisherbayb <alisherbayb@gmail.com>
2025-04-20 21:07:15 +0200

    Create proc_create_tables.sql

---
+++ scripts/bronze/proc_create_tables.sql
@@ -0,0 +1,91 @@
+/*
+===============================================================================
+Stored procedure: Create Bronze Tables
+===============================================================================
+Script Purpose:
+    This script creates tables in the 'bronze' schema, dropping existing tables 
+    if they already exist.
+	  Run this script to re-define the DDL structure of 'bronze' Tables
+===============================================================================
+*/
+
+-- PROCEDURE: bronze.create_tables()
+
+-- DROP PROCEDURE IF EXISTS bronze.create_tables();
+
+CREATE OR REPLACE PROCEDURE bronze.create_tables(
+	)
+LANGUAGE 'plpgsql'
+AS $BODY$
+BEGIN
+	EXECUTE 'DROP TABLE IF EXISTS bronze.crm_cust_info';
+	EXECUTE '
+		CREATE TABLE bronze.crm_cust_info (
+			cst_id INT,
+			cst_key VARCHAR(50),
+			cst_firstname VARCHAR(50),
+			cst_lastname VARCHAR(50),
+			cst_marital_status VARCHAR(50),
+			cst_gndr VARCHAR(50),
+			cst_create_date DATE
+		)
+	';
+
+	EXECUTE 'DROP TABLE IF EXISTS bronze.crm_prd_info';
+	EXECUTE '
+		CREATE TABLE bronze.crm_prd_info (
+			prd_id INT,
+			prd_key VARCHAR(50),
+			prd_nm VARCHAR(50),
+			prd_cost INT,
+			prd_line VARCHAR(50),
+			prd_start_dt TIMESTAMP,
+			prd_end_dt TIMESTAMP
+		)
+	';
+
+	EXECUTE 'DROP TABLE IF EXISTS bronze.crm_sales_details';
+	EXECUTE '
+		CREATE TABLE bronze.crm_sales_details (
+			sls_ord_num VARCHAR(50),
+			sls_prd_key VARCHAR(50),
+			sls_cust_id INT,
+			sls_order_dt INT,
+			sls_ship_dt INT,
+			sls_due_dt INT,
+			sls_sales INT,
+			sls_quantity INT,
+			sls_price INT
+		)
+	';
+
+	EXECUTE 'DROP TABLE IF EXISTS bronze.erp_loc_a101';
+	EXECUTE '
+		CREATE TABLE bronze.erp_loc_a101 (
+			cid VARCHAR(50),
+			cntry VARCHAR(50)
+		)
+	';
+
+	EXECUTE 'DROP TABLE IF EXISTS bronze.erp_cust_az12';
+	EXECUTE '
+		CREATE TABLE bronze.erp_cust_az12 (
+			cid VARCHAR(50),
+			bdate DATE,
+			gen VARCHAR(50)
+		)
+	';
+
+	EXECUTE 'DROP TABLE IF EXISTS bronze.erp_px_cat_g1v2';
+	EXECUTE '
+		CREATE TABLE bronze.erp_px_cat_g1v2 (
+			id VARCHAR(50),
+			cat VARCHAR(50),
+			subcat VARCHAR(50),
+			maintenance VARCHAR(50)
+		)
+	';
+END;
+$BODY$;
+ALTER PROCEDURE bronze.create_tables()
+    OWNER TO postgres;
