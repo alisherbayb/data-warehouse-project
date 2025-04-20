4f3464ebc79855b84c8d6040086009dd9bfb0fee
alisherbayb <alisherbayb@gmail.com>
2025-04-20 21:09:37 +0200

    Create proc_load.sql

---
+++ scripts/bronze/proc_load.sql
@@ -0,0 +1,150 @@
+/*
+===============================================================================
+Stored Procedure: Load Bronze Layer (Source -> Bronze)
+===============================================================================
+Script Purpose:
+    This stored procedure loads data into the 'bronze' schema from external CSV files. 
+    It performs the following actions:
+    - Truncates the bronze tables before loading data.
+    - Uses the `COPY FROM` command to load data from csv files to bronze tables.
+
+Parameters:
+    None. 
+	  This stored procedure does not accept any parameters or return any values.
+
+Usage Example:
+     CALL bronze.load_bronze();
+===============================================================================
+*/
+
+-- PROCEDURE: bronze.load_bronze()
+
+-- DROP PROCEDURE IF EXISTS bronze.load_bronze();
+
+CREATE OR REPLACE PROCEDURE bronze.load_bronze(
+	)
+LANGUAGE 'plpgsql'
+AS $BODY$
+DECLARE
+	start_time TIMESTAMP;
+	end_time TIMESTAMP;
+BEGIN
+	RAISE NOTICE 'Loading Bronze Layer';
+
+	RAISE NOTICE 'Loading CRM Tables';
+
+	BEGIN
+		start_time := clock_timestamp();
+		RAISE NOTICE '>> Truncating Table: bronze.crm_cust_info';
+		TRUNCATE TABLE BRONZE.CRM_CUST_INFO;
+		
+		RAISE NOTICE '>> Inserting Data Into: bronze.crm_cust_info';
+		COPY BRONZE.CRM_CUST_INFO
+		FROM '/Users/alisherbaibussinov/dev/DataWarehouseProject/datasets/source_crm/cust_info.csv'
+		DELIMITER ','
+		CSV HEADER;
+	
+		end_time := clock_timestamp();
+		RAISE NOTICE '>> Load Duration: % seconds', EXTRACT(epoch from (end_time - start_time));
+		
+		EXCEPTION
+			WHEN OTHERS THEN
+			RAISE WARNING 'Failed to load bronze.crm_cust_info: %', SQLERRM;
+	END;
+
+	BEGIN
+		start_time := clock_timestamp();
+		RAISE NOTICE '>> Truncating Table: bronze.crm_prd_info';
+		TRUNCATE TABLE BRONZE.CRM_PRD_INFO;
+	
+		RAISE NOTICE '>> Inserting Data Into: bronze.crm_prd_info';
+		COPY BRONZE.CRM_PRD_INFO
+		FROM '/Users/alisherbaibussinov/dev/DataWarehouseProject/datasets/source_crm/prd_info.csv'
+		DELIMITER ','
+		CSV HEADER;
+	
+		end_time := clock_timestamp();
+		RAISE NOTICE '>> Load Duration: % seconds', EXTRACT(epoch from (end_time - start_time));
+
+		EXCEPTION
+			WHEN OTHERS THEN
+			RAISE WARNING 'Failed to load bronze.crm_cust_info: %', SQLERRM;
+	END;
+
+	BEGIN
+		start_time := clock_timestamp();
+		RAISE NOTICE '>> Truncating Table: bronze.crm_sales_info';
+		TRUNCATE TABLE BRONZE.CRM_SALES_DETAILS;
+	
+		RAISE NOTICE '>> Inserting Data Into: bronze.crm_sales_info';
+		COPY BRONZE.CRM_SALES_DETAILS
+		FROM '/Users/alisherbaibussinov/dev/DataWarehouseProject/datasets/source_crm/sales_details.csv'
+		DELIMITER ','
+		CSV HEADER;
+	
+		end_time := clock_timestamp();
+		RAISE NOTICE '>> Load Duration: % seconds', EXTRACT(epoch from (end_time - start_time));
+		EXCEPTION
+			WHEN OTHERS THEN
+			RAISE WARNING 'Failed to load bronze.crm_cust_info: %', SQLERRM;
+	END;
+
+	RAISE NOTICE 'Loading ERP Tables';
+
+	BEGIN
+		start_time := clock_timestamp();
+		RAISE NOTICE '>> Truncating Table: bronze.erp_cust_az12';
+		TRUNCATE TABLE BRONZE.ERP_CUST_AZ12;
+	
+		RAISE NOTICE '>> Inserting Data Into: bronze.erp_cust_az12';
+		COPY BRONZE.ERP_CUST_AZ12
+		FROM '/Users/alisherbaibussinov/dev/DataWarehouseProject/datasets/source_erp/cust_az12.csv'
+		DELIMITER ','
+		CSV HEADER;
+	
+		end_time := clock_timestamp();
+		RAISE NOTICE '>> Load Duration: % seconds', EXTRACT(epoch from (end_time - start_time));
+		EXCEPTION
+				WHEN OTHERS THEN
+				RAISE WARNING 'Failed to load bronze.crm_cust_info: %', SQLERRM;
+	END;
+
+	BEGIN
+		start_time := clock_timestamp();
+		RAISE NOTICE '>> Truncating Table: bronze.erp_loc_a101';
+		TRUNCATE TABLE BRONZE.ERP_LOC_A101;
+	
+		RAISE NOTICE '>> Inserting Data Into: bronze.erp_loc_a101';
+		COPY BRONZE.ERP_LOC_A101
+		FROM '/Users/alisherbaibussinov/dev/DataWarehouseProject/datasets/source_erp/loc_a101.csv'
+		DELIMITER ','
+		CSV HEADER;
+	
+		end_time := clock_timestamp();
+		RAISE NOTICE '>> Load Duration: % seconds', EXTRACT(epoch from (end_time - start_time));
+		EXCEPTION
+				WHEN OTHERS THEN
+				RAISE WARNING 'Failed to load bronze.crm_cust_info: %', SQLERRM;
+	END;
+
+	BEGIN
+		start_time := clock_timestamp();
+		RAISE NOTICE '>> Truncating Table: bronze.erp_px_cat_g1v2';
+		TRUNCATE TABLE BRONZE.ERP_PX_CAT_G1V2;
+	
+		RAISE NOTICE '>> Inserting Data Into: bronze.erp_px_cat_g1v2';
+		COPY BRONZE.ERP_PX_CAT_G1V2
+		FROM '/Users/alisherbaibussinov/dev/DataWarehouseProject/datasets/source_erp/px_cat_g1v2.csv'
+		DELIMITER ','
+		CSV HEADER;
+	
+		end_time := clock_timestamp();
+		RAISE NOTICE '>> Load Duration: % seconds', EXTRACT(epoch from (end_time - start_time));
+		EXCEPTION
+				WHEN OTHERS THEN
+				RAISE WARNING 'Failed to load bronze.crm_cust_info: %', SQLERRM;
+	END;
+END;
+$BODY$;
+ALTER PROCEDURE bronze.load_bronze()
+    OWNER TO postgres;
